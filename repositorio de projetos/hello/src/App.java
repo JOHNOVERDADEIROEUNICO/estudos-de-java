2b02dc08d8fee2c322da695ed339fb26cdaa0371
JOHNOVERDADEIROEUNICO <joaopedroteixeira26@gmail.com>
2026-04-24 11:46:50 -0300

    aprendendo a sintaxe java

---
+++ repositorio de projetos/hello/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Configurando o JDK na versão " + args[0]);
+        System.out.println("Progamando em " + args[1]);
+    }
+}
